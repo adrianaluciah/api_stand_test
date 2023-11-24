afcd50b2a25f44f8c2208d362e7195c26bf05460
adriana <adrianaherazo25@gmail.com>
2023-11-24 18:39:05 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+import data
+
+URL_SERVICE = "https://c8dc707a-fa38-479b-910e-678b7847981d.serverhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
